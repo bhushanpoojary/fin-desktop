98675d41b87581c11c40a03b0ec81a351180e412
bhushanpoojary@gmail.com <kaneticmomentum-dev@outlook.com>
2025-12-04 22:50:12 +0530

    hard coded values moved to config

--- fin-desktop/electron/workspaceWindow.js
+++ fin-desktop/electron/workspaceWindow.js
@@ -1,6 +1,8 @@
 const { BrowserWindow } = require('electron');
 const path = require('node:path');
 
+const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL || 'http://localhost:5173';
+
 async function createWorkspaceWindow() {
   const win = new BrowserWindow({
     width: 1600,
@@ -10,7 +12,7 @@ async function createWorkspaceWindow() {
     }
   });
 
-  await win.loadURL('http://localhost:5173/?entry=workspace');
+  await win.loadURL(`${VITE_DEV_SERVER_URL}/?entry=workspace`);
   
   return win;
 }
@@ -24,7 +26,7 @@ async function createAppWindow(appId) {
     }
   });
 
-  await win.loadURL(`http://localhost:5173/?entry=app&appId=${encodeURIComponent(appId)}`);
+  await win.loadURL(`${VITE_DEV_SERVER_URL}/?entry=app&appId=${encodeURIComponent(appId)}`);
   
   return win.id;
 }
